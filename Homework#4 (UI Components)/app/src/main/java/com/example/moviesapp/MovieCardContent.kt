490a440c7236cc48184937c0ba68a6d1485e9251
Nick Leonov <krotghar@gmail.com>
2020-12-27 09:08:52 +0300

    backup

---
+++ Homework#4 (UI Components)/app/src/main/java/com/example/moviesapp/MovieCardContent.kt
@@ -0,0 +1,10 @@
+package com.example.moviesapp
+
+data class MovieCardContent(val titleMovie: String,
+                            val pg: String,
+                            val bgRef: Int,
+                            val genres: String,
+                            val reviews: String,
+                            val time: String,
+                            val rating: Float
+                            )
